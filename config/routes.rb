2debc1e44340ebc8a1d60697f5e737d5e319529b
axaidan <axaidan@hotmail.com>
2020-02-18 16:35:35 +0100

    MAJ HEROKU

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
 	root 'events#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	
+	# EVENTS
 	resources :events
+
+	# USERS COMPLEMENT - ONLY SHOW
+	resources :users, only: [:index, :show, :edit, :update]
 end
