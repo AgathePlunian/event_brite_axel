4e645b390ae7867a70274920d260eed7b330d43f
axaidan <axaidan@hotmail.com>
2020-02-18 23:18:55 +0100

    fin de journee

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+	def show
+		puts '=' * 25 + 'users#show' + '=' * 25
+		puts params
+		puts '=' * 25 + 'users#show' + '=' * 25
+		@user = User.find(params[:id])
+	end
 end
